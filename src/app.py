b53a403814edd25c9659f0ca27e73b28e8f72bca
local angoies <jgomez@iesromerovargas.com>
2025-05-05 17:03:43 +0200

    test para repaso

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ locale.setlocale(locale.LC_TIME, '')
 
 @app.route('/')
 def homepage():
-    xyz = "variable no usada para error linter"
+    xyzw = "variable no usada para error linter"
     the_time = datetime.now().strftime("%A, %d %b %Y %H:%M")
 
     # Render HTML with variable
